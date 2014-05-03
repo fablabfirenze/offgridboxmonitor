375e8a09447020ebe51010536e02833c3687c95a
tommaso rossi <kurtommy@gmail.com>
2014-05-03 12:50:37 +0200

    Interfaccia

--- offgridboxmonitor.ino
+++ offgridboxmonitor.ino
@@ -1,12 +1,24 @@
-//
+//DISPLAY CONST
 const int DISPLAY_SDA = 20;  
 const int DISPLAY_SDL = 21;
 
+//GSM MODULE CONST
+const int GSM_RX = 2;
+const int GSM_TX = 3;
+const int GSM_RESET = 7;
+
+//SD CONST
+const int SD_SPI_1 = 2;  
+const int SD_SPI_2 = 3;
+const int SD_SPI_2 = 3;
+
+//PUBLIC VARS
 float panelTemp      = 0.0;
 float ambientTemp    = 0.0;
 float batteryVoltage = 0.0;
 float waterLevel     = 0.0;
 
+//MIX
 boolean readSensorValue = true;
 boolean sendValueToServer = true;
 
@@ -15,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-  //Timer lettura sensori
+  //READ SENSOR VALUES
   if(readSensorValue){
     panelTemp      = getPanelTemp();
     ambientTemp    = getAmbientTemp();
@@ -25,28 +37,35 @@ void loop() {
   }
   
   if(sendValueToServer){
-   //invio dati al server
+   //SEND DATA TO THE SERVER
   }
   
   delay(2000);
 }
 
+//READ PANEL TEMPERATURE
+//MICHELE - FRANCESCO
 float getPanelTemp(){
   return 1; 
 }
 
+//READ AMBIENT TEMPERATURE
+//MICHELE - FRANCESCO
 float getAmbientTemp(){
   return 1; 
 }
 
+//READ BATTERY TEMPERATURE (24 V BATTERY)
 float getBatteryVoltage(){
   return 1; 
 }
 
+//READ WATER LEVEL (MAX TANK LEVEL 1200LT)
 float getWaterLevel(){
   return 1; 
 }
 
+//UPDATE DISPLAY VALUES
 void updateDisplayValues(){
   
 }
