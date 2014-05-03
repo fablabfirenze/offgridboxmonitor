ba3e7f67c298576d836a265a07284c392294de4c
tommaso rossi <kurtommy@gmail.com>
2014-05-03 13:31:09 +0200

    test

--- offgridboxmonitor.ino
+++ offgridboxmonitor.ino
@@ -12,7 +12,7 @@ const int SD_SPI_1 = 2;
 const int SD_SPI_2 = 3;
 const int SD_SPI_2 = 3;
 
-//PUBLIC VARS
+//PUBLIC VARS test
 float panelTemp          = 0.0;
 float ambientTemp        = 0.0;
 float batteryVoltage     = 0.0;
