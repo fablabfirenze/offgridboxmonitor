79c4e00693a52abeb0a5ce7636ff4c15b52f901d
fleoncino <francesco@leoncino.net>
2014-05-04 10:41:33 +0200

    Temp sensor

--- offgridboxmonitor.ino
+++ offgridboxmonitor.ino
@@ -1,13 +1,22 @@
+#include "Adafruit_MAX31855.h"
+
+//THERMOCOUPLE AMBIENT CONST
+const int THERMO_ENV_DO = 22;//13
+const int THERMO_ENV_CS = 24;//11
+const int THERMO_ENV_CLK =26;//12
+
+//THERMOCOUPLE PANEL CONST
+const int THERMO_PANEL_DO = 23;//10
+const int THERMO_PANEL_CS = 25;//8
+const int THERMO_PANEL_CLK = 27;//9
+
+//ADAFRUIT INTERFACE OBJECT DECLARATION 
+Adafruit_MAX31855 thermocoupleEnvironment(THERMO_ENV_CLK, THERMO_ENV_CS, THERMO_ENV_DO);
+Adafruit_MAX31855 thermocouplePanel(THERMO_PANEL_CLK, THERMO_PANEL_CS, THERMO_PANEL_DO);
 
 //DISPLAY CONST
-const int LCD_SDA = 20;
-const int LCD_SDL = 21;
-
-// Button Pin
-const int  LCD_Button = 4;
-
-//WATER CONST
-const int WATER_LEVEL_PIN = A0;
+const int DISPLAY_SDA = 20;  
+const int DISPLAY_SDL = 21;
 
 //GSM MODULE CONST
 const int GSM_RX = 2;
@@ -15,9 +24,8 @@ const int GSM_TX = 3;
 const int GSM_RESET = 7;
 
 //SD CONST
-const int SD_SPI_1 = 2;
+const int SD_SPI_1 = 2;  
 const int SD_SPI_2 = 3;
-const int SD_SPI_3 = 3;
 
 //PUBLIC VARS
 float panelTemp          = 0.0;
@@ -25,14 +33,18 @@ float ambientTemp        = 0.0;
 float batteryVoltage     = 0.0;
 int   batteryPercentage  = 0.0;
 float waterLevel         = 0.0;
+boolean isEnvironmentTempValid = false;  //true:indicate valid temp number; fales: temp read failed NAN 
+boolean isPanelTempValid = false;  //true:indicate valid temp number; fales: temp read failed NAN
+boolean debug = true;              //true:fro printing data in serial line
 
 //MIX
 boolean readSensorValue = true;
 boolean sendValueToServer = true;
 
 void setup() {
-  Serial.begin(9600);
-  setupLCD();
+  if(debug == true){
+    Serial.begin(9600); 
+  }
 }
 
 void loop() {
@@ -43,70 +55,91 @@ void loop() {
     batteryVoltage    = getBatteryVoltage();
     batteryPercentage = getBatteryPercentage();
     waterLevel        = getWaterLevel();
-    
+    updateDisplayValues();
   }
-
+  
   if(sendValueToServer){
    //SEND DATA TO THE SERVER
   }
-
-  LCD(digitalRead(LCD_BUTTON));
-
-  //delay(2000);
+  
+  delay(2000);
 }
 
 //READ PANEL TEMPERATURE
 //MICHELE - FRANCESCO
 float getPanelTemp(){
-  return 1;
+
+  if(debug == true){
+   Serial.print("Internal Panel Temp = ");
+   Serial.println(thermocouplePanel.readInternal());
+  }
+
+   float c = (float)thermocouplePanel.readCelsius();
+   
+   if (isnan(c)) {
+     isPanelTempValid = false;
+     if(debug == true){
+       Serial.println("Something wrong with Panel thermocouple!");
+     }
+   } else {
+     isPanelTempValid = true;
+     if(debug == true){
+       Serial.print("PanelTemp = "); 
+       Serial.println(c);
+     }
+   }
+   if(debug == true){
+     delay(1000);
+   }
+   return c;
 }
 
 //READ AMBIENT TEMPERATURE
 //MICHELE - FRANCESCO
 float getAmbientTemp(){
-  return 1;
+
+  if(debug == true){
+   Serial.print("Internal Environment Temp = ");
+   Serial.println(thermocoupleEnvironment.readInternal());
+  }
+
+   float c = (float)thermocoupleEnvironment.readCelsius();
+   
+   if (isnan(c)) {
+     isEnvironmentTempValid = false;
+     if(debug == true){
+       Serial.println("Something wrong with Environment thermocouple!");
+     }
+   } else {
+     isEnvironmentTempValid = true;
+     if(debug == true){
+       Serial.print("ambientTemp = "); 
+       Serial.println(c);
+     }
+   }
+   if(debug == true){
+     delay(1000);
+   }
+   return c;
 }
+
+
 
 //READ BATTERY TEMPERATURE (24 V BATTERY)
 float getBatteryVoltage(){
-  return 1;
+  return 1; 
 }
 //READ BATTERY PERCENTAGE
 int getBatteryPercentage(){
   return 1; 
 }
 
-//READ WATER LEVEL (MAX TANK LEVEL 1500LT)
+//READ WATER LEVEL (MAX TANK LEVEL 1200LT)
 float getWaterLevel(){
-
-  float hmax = 95; // [cm] ch 1500 specification  (max value using formula)
-  float Vempty = 1.30; //[V] @h = 0 
-  float Vfull = 2.33; //[V] @h = hmax
-  float Delta = (Vfull-Vempty)/hmax; // [V]/[cm]
-  float L = 163; //tank length CH 1500 [cm]
-  float R = 57.75; //radius tank CH 1500 [cm]
- 
-  int sensorValue = analogRead(A0);
-  float Vactual = sensorValue * (5.0 / 1023.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
- 
-  //float V = Vempty+Delta*h;
-  float h = (Vactual-Vempty)/Delta; //normalized height
-  
-  float liters = L*(pow(R,2)*acos((R-h)/R)+ (h-R)*sqrt( 2*h*R - pow(h,2)))/1000; //volume = area under the arc * length --> 1dm^3 = 1000cm^3 
-
-  if(h > 95){ //check max liters = 1500 @95cm as in the formula
-    liters = 1500; 
-  }
-  
-  if (h < 2.82) { //chack min liters @h=2.82cm
-     h = 0;
-     liters = 0;     
-  }
-  
-  return liters;
+  return 1; 
 }
 
 //UPDATE DISPLAY VALUES
 void updateDisplayValues(){
-
+  
 }
