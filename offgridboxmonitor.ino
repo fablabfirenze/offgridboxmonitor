5c0da8829329fdd0e1966fe44325f2a041d845b0
atomm <tommaso.agnoloni@gmail.com>
2014-05-04 12:16:26 +0200

    getWaterLevel implementation

--- offgridboxmonitor.ino
+++ offgridboxmonitor.ino
@@ -10,6 +10,11 @@ const int THERMO_PANEL_DO = 23;//10
 const int THERMO_PANEL_CS = 25;//8
 const int THERMO_PANEL_CLK = 27;//9
 
+
+//WATERLEVEL  CONST
+const int WATER_LEVEL_SENSOR = A0;
+
+
 //ADAFRUIT INTERFACE OBJECT DECLARATION 
 Adafruit_MAX31855 thermocoupleEnvironment(THERMO_ENV_CLK, THERMO_ENV_CS, THERMO_ENV_DO);
 Adafruit_MAX31855 thermocouplePanel(THERMO_PANEL_CLK, THERMO_PANEL_CS, THERMO_PANEL_DO);
@@ -134,9 +139,32 @@ int getBatteryPercentage(){
   return 1; 
 }
 
-//READ WATER LEVEL (MAX TANK LEVEL 1200LT)
+//READ WATER LEVEL (MAX TANK LEVEL 1500LT)
+// FRANCESCA - TOMMASO
 float getWaterLevel(){
-  return 1; 
+  
+  float tankHeight = 123; // [cm] total height excursion
+ 
+  float Vempty = 1.30; //[V] @h = 0
+  float Vfull = 2.61; //[V] @h = tankHeight
+  float Delta = (Vfull-Vempty)/tankHeight; // [V]/[cm]
+  
+  int sensorValue = analogRead(WATER_LEVEL_SENSOR);
+  float Vactual = sensorValue * (5.0 / 1023.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
+ 
+  float h = (Vactual-Vempty)/Delta; //normalized height
+  
+
+  // VOLUME COMPUTATION
+  float hmax = 95; // [cm] CH 1500 TANK specification  (height at which TANK volume is 1500 liters - max value using formula for liters)
+  float L = 163; //tank length CH 1500 [cm]
+  float R = 57.75; //radius tank CH 1500 [cm]
+  float liters = L*(pow(R,2)*acos((R-h)/R)+ (h-R)*sqrt( 2*h*R - pow(h,2)))/1000; //volume = area under the arc * length --> 1dm^3 = 1000cm^3 
+
+  if(h>=2*R){ //check max liters = 1500 @2*R as in the formula
+    liters = 1500; 
+  } 
+  return liters;
 }
 
 //UPDATE DISPLAY VALUES
